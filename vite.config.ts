6bc65ac6287210c28fe8d9d5956a7d81d856292b
Angel <angel.santiago@tutanota.com>
2023-04-25 08:03:25 -0400

    Fix absolute pathing for imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
+const resolve = (...dirs) => path.resolve(__dirname, '../../', ...dirs);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      game: resolve('./src/game')
+    },
+  },
 })
